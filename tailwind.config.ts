ff789f68de480c7202a4ac7cfcbab7b79b854d95
Tina Huynh <tinamchuynh@gmail.com>
2025-02-23 21:45:20 -0800

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,144 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			tertiary: {
+  				DEFAULT: 'var(--tertiary)',
+  				foreground: 'var(--tertiary-foreground)'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				'1': 'var(--accent-1)',
+  				'2': 'var(--accent-2)',
+  				'3': 'var(--accent-3)',
+  				'4': 'var(--accent-4)',
+  				'5': 'var(--accent-5)',
+  				foreground: 'hsl(var(--accent-foreground))',
+  				DEFAULT: 'hsl(var(--accent))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'var(--sidebar-background)',
+  				foreground: 'var(--sidebar-foreground)',
+  				primary: 'var(--sidebar-primary)',
+  				'primary-foreground': 'var(--sidebar-primary-foreground)',
+  				accent: 'var(--sidebar-accent)',
+  				'accent-foreground': 'var(--sidebar-accent-foreground)',
+  				border: 'var(--sidebar-border)',
+  				ring: 'var(--sidebar-ring)'
+  			}
+  		},
+  		fontFamily: {
+  			RampartOne: [
+  				'RampartOne',
+  				'sans-serif'
+  			],
+  			SofiaSans: [
+  				'SofiaSans',
+  				'sans-serif'
+  			],
+  			Ruthie: [
+  				'Ruthie',
+  				'cursive'
+  			],
+  			RedRose: [
+  				'RedRose',
+  				'sans-serif'
+  			],
+  			Notable: [
+  				'Notable',
+  				'sans-serif'
+  			],
+  			Dosis: [
+  				'Dosis',
+  				'sans-serif'
+  			],
+  			Corinthia: [
+  				'Corinthia',
+  				'cursive'
+  			],
+  			Buda: [
+  				'Buda',
+  				'serif'
+  			],
+  			BonaNovaSC_Bold: [
+  				'BonaNovaSC_Bold',
+  				'serif'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		},
+  		listStyleType: {
+  			roman: 'upper-roman'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
