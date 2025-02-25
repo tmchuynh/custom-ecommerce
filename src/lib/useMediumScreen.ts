8592c9959899434ecc84f6a6f59c40cfdee3cf98
Tina Huynh <tinamchuynh@gmail.com>
2025-02-24 21:42:24 -0800

    feat: add custom hooks for medium and small screen size detection

---
+++ src/lib/useMediumScreen.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+/**
+ * Custom hook to determine if the medium size is small (<= 768px).
+ */
+const useMediumScreen = () => {
+  const [IsMediumScreen, setIsMediumScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMediumScreen(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return IsMediumScreen;
+};
+
+export default useMediumScreen;
