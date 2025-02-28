44fa15752d69079729fd3f64b8ecc30c4e0c909f
Tina Huynh <tinamchuynh@gmail.com>
2025-02-27 22:22:09 -0800

    refactor: remove redundant class from LoginPage layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/loginform";
 import { GalleryVerticalEnd } from "lucide-react";
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
