570b03b22d5ae2eceadb3daec23765f9513c89ef
Tina Huynh <tinamchuynh@gmail.com>
2025-03-02 20:46:08 -0800

    feat: implement loading state in dashboard page with timeout effect

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,27 +1,19 @@
-import { SidebarRight } from "@/components/ui/navbar-right";
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
-import { SidebarLeft } from "@/components/ui/sidebar-left";
+"use client";
+import { useEffect, useState } from "react";
 
 export default function Page() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  });
+
   return (
-    <SidebarProvider>
-      <SidebarLeft />
-      <SidebarInset>
-        <header className="sticky top-0 flex h-14 shrink-0 items-center gap-2 bg-background">
-          <div className="flex flex-1 items-center gap-2 px-3">
-            <SidebarTrigger />
-          </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="mx-auto h-24 w-full max-w-3xl rounded-xl bg-muted/50" />
-          <div className="mx-auto h-[100vh] w-full max-w-3xl rounded-xl bg-muted/50" />
-        </div>
-      </SidebarInset>
-      <SidebarRight />
-    </SidebarProvider>
+    <>
+      <div className="mx-auto h-24 w-full max-w-3xl rounded-xl bg-muted/50" />
+      <div className="mx-auto h-[100vh] w-full max-w-3xl rounded-xl bg-muted/50" />
+    </>
   );
 }
