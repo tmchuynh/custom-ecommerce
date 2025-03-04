fb23e670db714f49f532f8a1b1945e7af92d29a7
Tina Huynh <tinamchuynh@gmail.com>
2025-03-03 22:32:55 -0800

    refactor: update LoginPage layout to use <main> for semantic structure and improve LoginForm import

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,8 @@
-import { LoginForm } from "@/components/loginform";
+import LoginForm from "@/components/loginform";
 import { GalleryVerticalEnd } from "lucide-react";
 export default function LoginPage() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <main className="flex flex-col items-center justify-center gap-6 p-6 md:p-10 lg:py-16 w-11/12 md:w-10/12 mx-auto">
       <div className="flex w-full flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
@@ -12,6 +12,6 @@ export default function LoginPage() {
         </a>
         <LoginForm />
       </div>
-    </div>
+    </main>
   );
 }
