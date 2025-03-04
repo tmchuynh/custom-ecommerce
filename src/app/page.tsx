d4cb68c95bbdb13c3d53d6adeda4d847d9153f03
Tina Huynh <tinamchuynh@gmail.com>
2025-03-03 22:28:11 -0800

    refactor: replace div with main for semantic HTML in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import { Carousels } from "@/components/Carousel";
 export default function Home() {
   return (
     <>
-      <div>
+      <main className="w-11/12 md:w-10/12 mx-auto">
         <h1>Home Page</h1>
         <p>Welcome to the Home Page</p>
         <Carousels />
-      </div>
+      </main>
     </>
   );
 }
