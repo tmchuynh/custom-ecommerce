62d5e72338a53c18fc7a0fe69ae742a0e341d378
Tina Huynh <tinamchuynh@gmail.com>
2025-03-03 22:32:33 -0800

    refactor: update next.config.ts to allow images from placehold.co and tailwindui.com with SVG support

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,23 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: "template_bsky8la",
     NEXT_PUBLIC_EMAILJS_PUBLIC_KEY: "qeV_W2IMId2NMUL5W",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "", // Optional: You can leave this blank
+        pathname: "/**", // Allow all paths from placehold.co
+      },
+      {
+        protocol: "https",
+        hostname: "tailwindui.com", // Adding the tailwindui.com domain
+        port: "", // Optional: You can leave this blank
+        pathname: "/**", // Allow all paths from tailwindui.com
+      },
+    ],
+    dangerouslyAllowSVG: true, // Allows SVG images to be loaded
+  },
 };
 
 export default nextConfig;
