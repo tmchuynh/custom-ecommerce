9a1b678c4603894a2993a38f2310cb6672c173f6
Tina Huynh <tinamchuynh@gmail.com>
2025-03-05 21:52:20 -0800

    feat: enhance About page layout with icon integration and responsive adjustments

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,8 @@ import { JSX, SVGProps, useState } from "react";
 import Image from "next/image";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Icon } from "lucide-react";
+import { Fa500Px } from "react-icons/fa";
 
 const people = [
   {
@@ -133,31 +135,37 @@ const values = [
     name: "Be world-class",
     description:
       "Aut illo quae. Ut et harum ea animi natus. Culpa maiores et sed sint et magnam exercitationem quia. Ullam voluptas nihil vitae dicta molestiae et. Aliquid velit porro vero.",
+    Icon: Fa500Px,
   },
   {
     name: "Share everything you know",
     description:
       "Mollitia delectus a omnis. Quae velit aliquid. Qui nulla maxime adipisci illo id molestiae. Cumque cum ut minus rerum architecto magnam consequatur. Quia quaerat minima.",
+    Icon: Fa500Px,
   },
   {
     name: "Always learning",
     description:
       "Aut repellendus et officiis dolor possimus. Deserunt velit quasi sunt fuga error labore quia ipsum. Commodi autem voluptatem nam. Quos voluptatem totam.",
+    Icon: Fa500Px,
   },
   {
     name: "Be supportive",
     description:
       "Magnam provident veritatis odit. Vitae eligendi repellat non. Eum fugit impedit veritatis ducimus. Non qui aspernatur laudantium modi. Praesentium rerum error deserunt harum.",
+    Icon: Fa500Px,
   },
   {
     name: "Take responsibility",
     description:
       "Sit minus expedita quam in ullam molestiae dignissimos in harum. Tenetur dolorem iure. Non nesciunt dolorem veniam necessitatibus laboriosam voluptas perspiciatis error.",
+    Icon: Fa500Px,
   },
   {
     name: "Enjoy downtime",
     description:
       "Ipsa in earum deserunt aut. Quos minus aut animi et soluta. Ipsum dicta ut quia eius. Possimus reprehenderit iste aspernatur ut est velit consequatur distinctio.",
+    Icon: Fa500Px,
   },
 ];
 const team = [
@@ -416,11 +424,19 @@ export default function Example() {
             voluptatum cupiditate veritatis in accusamus quisquam.
           </p>
         </div>
-        <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-base/7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-base/7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-2 xl:grid-cols-3">
           {values.map((value) => (
-            <div key={value.name}>
-              <dt className="font-semibold text-gray-900">{value.name}</dt>
-              <dd className="mt-1 text-gray-600">{value.description}</dd>
+            <div
+              key={value.name}
+              className="flex flex-col gap-5 lg:flex-row lg:gap-8 xl:gap-4 2xl:flex-col"
+            >
+              <value.Icon className="text-[5em] md:text-[7em] lg:text-[30em] lg:my-auto xl:text-[20em] 2xl:text-[6em] xl:mt-11 h-fit" />
+              <div className="flex flex-col h-full justify-between">
+                <dt className="font-bold uppercase text-primary md:tracking-wide lg:tracking-wider xl:tracking-widest text-xl">
+                  {value.name}
+                </dt>
+                <dd className="mt-1 text-gray-600">{value.description}</dd>
+              </div>
             </div>
           ))}
         </dl>
