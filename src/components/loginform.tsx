a5f9a157973e3777fe1440abb6c218c79061a39b
Tina Huynh <tinamchuynh@gmail.com>
2025-03-05 20:11:38 -0800

    feat: remove registration form and related logic from LoginPage component

--- src/components/loginform.tsx
+++ src/components/loginform.tsx
@@ -8,33 +8,14 @@ import React, { useEffect, useState } from "react";
 import { CoolMode } from "./magicui/cool-mode";
 import { Sonner } from "./Sonner";
 import Link from "next/link";
-import {
-  HoverCard,
-  HoverCardTrigger,
-  HoverCardContent,
-} from "@/components/ui/hover-card";
 
 const LoginPage: React.FC = () => {
   const router = useRouter();
-  const [userData, setUserData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    username: "",
-    password: "",
-    confirmPassword: "",
-  });
   const [toastMessage, setToastMessage] = useState<{
     type: "error" | "success";
     message: string;
   } | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [passwordValidations, setPasswordValidations] = useState({
-    length: false,
-    number: false,
-    symbol: false,
-  });
-  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
 
   useEffect(() => {
     if (!sessionStorage.getItem("reloaded")) {
@@ -48,88 +29,6 @@ const LoginPage: React.FC = () => {
     setTimeout(() => {
       setToastMessage(null);
     }, 5000);
-  };
-
-  const checkAvailability = async (data: any) => {
-    const { username } = data;
-
-    try {
-      const response = await fetch(`/api/users?username=${username}`, {
-        credentials: "include",
-      });
-
-      if (response.ok) {
-        showToast(
-          "error",
-          "Username already exists in the database. Try logging in."
-        );
-      } else {
-        handleRegister(data);
-      }
-    } catch (e) {
-      console.error(e);
-      showToast(
-        "error",
-        "An error occurred while checking username availability."
-      );
-    }
-  };
-
-  const handleRegister = async (data: any) => {
-    const { firstName, lastName, username, password, email } = data;
-
-    if (password !== userData.confirmPassword) {
-      showToast("error", "Passwords do not match.");
-      return;
-    }
-
-    if (
-      !passwordValidations.length ||
-      !passwordValidations.number ||
-      !passwordValidations.symbol
-    ) {
-      showToast("error", "Password does not meet the required criteria.");
-      return;
-    }
-
-    setIsLoading(true);
-
-    try {
-      const response = await fetch("/api/auth/register", {
-        method: "POST",
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          full_name: `${firstName} ${lastName}`,
-          username,
-          password,
-          email,
-          image: "",
-        }),
-      });
-
-      if (!response.ok) {
-        showToast("error", "Registration failed. Please try again later.");
-      } else {
-        showToast("success", "Registration successful! Redirecting...");
-        setUserData({
-          firstName: "",
-          lastName: "",
-          email: "",
-          username: "",
-          password: "",
-          confirmPassword: "",
-        });
-        router.push("/login");
-      }
-    } catch (error) {
-      console.error("Error during registration:", error);
-      showToast("error", "An error occurred during registration.");
-    } finally {
-      setIsLoading(false);
-    }
   };
 
   const handleLogin = async (username: string, password: string) => {
@@ -179,22 +78,6 @@ const LoginPage: React.FC = () => {
       );
     } finally {
       setIsLoading(false);
-    }
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setUserData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-
-    if (name === "password") {
-      setPasswordValidations({
-        length: value.length >= 8,
-        number: /\d/.test(value),
-        symbol: /[!@#$%^&*(),.?":{}|<>]/.test(value),
-      });
     }
   };
 
@@ -251,145 +134,6 @@ const LoginPage: React.FC = () => {
             <CoolMode options={{ particleCount: 50 }}>
               <Button size="lg" disabled={isLoading}>
                 {isLoading ? "Logging in..." : "Login"}
-              </Button>
-            </CoolMode>
-          </form>
-        </div>
-        {/* Registration Form */}
-        <div className="p-6 rounded-lg shadow-md border hover:shadow-md">
-          <h2 className="text-center">Register</h2>
-          <form
-            className="mx-auto w-full p-10"
-            onSubmit={(e) => {
-              e.preventDefault();
-              checkAvailability(userData);
-            }}
-          >
-            <div className="grid md:grid-cols-2 md:gap-6">
-              {/* First Name */}
-              <div className="relative z-0 w-full mb-5 group">
-                <Label htmlFor="register_fName">First Name</Label>
-                <Input
-                  id="register_fName"
-                  type="text"
-                  name="firstName"
-                  value={userData.firstName}
-                  onChange={handleInputChange}
-                  placeholder="First name"
-                  required
-                />
-              </div>
-              {/* Last Name */}
-              <div className="relative z-0 w-full mb-5 group">
-                <Label htmlFor="register_lName">Last Name</Label>
-                <Input
-                  id="register_lName"
-                  type="text"
-                  name="lastName"
-                  value={userData.lastName}
-                  onChange={handleInputChange}
-                  placeholder="Last name"
-                  required
-                />
-              </div>
-            </div>
-            {/* Username */}
-            <div className="relative z-0 w-full mb-5 group">
-              <Label htmlFor="register_username">Username</Label>
-              <Input
-                id="register_username"
-                type="text"
-                name="username"
-                value={userData.username}
-                onChange={handleInputChange}
-                placeholder="Username"
-                required
-                autoComplete="username"
-              />
-            </div>
-            {/* Email */}
-            <div className="relative z-0 w-full mb-5 group">
-              <Label htmlFor="register_email">Email Address</Label>
-              <Input
-                id="register_email"
-                type="email"
-                name="email"
-                value={userData.email}
-                onChange={handleInputChange}
-                placeholder="Email address"
-                required
-                autoComplete="email"
-              />
-            </div>
-            {/* Password and Confirm Password */}
-            <div className="grid md:grid-cols-2 md:gap-6">
-              <div className="relative z-0 w-full mb-5 group">
-                <Label htmlFor="register_password">Password</Label>
-                <HoverCard open={isPasswordFocused}>
-                  <HoverCardTrigger asChild>
-                    <Input
-                      id="register_password"
-                      type="password"
-                      name="password"
-                      value={userData.password}
-                      onChange={handleInputChange}
-                      onFocus={() => setIsPasswordFocused(true)}
-                      onBlur={() => setIsPasswordFocused(false)}
-                      placeholder="Password"
-                      required
-                      autoComplete="new-password"
-                    />
-                  </HoverCardTrigger>
-                  <HoverCardContent>
-                    <div className="text-sm mt-2">
-                      <p
-                        className={
-                          passwordValidations.length
-                            ? "text-green-500"
-                            : "text-red-500"
-                        }
-                      >
-                        At least 8 characters
-                      </p>
-                      <p
-                        className={
-                          passwordValidations.number
-                            ? "text-green-500"
-                            : "text-red-500"
-                        }
-                      >
-                        At least one number
-                      </p>
-                      <p
-                        className={
-                          passwordValidations.symbol
-                            ? "text-green-500"
-                            : "text-red-500"
-                        }
-                      >
-                        At least one symbol
-                      </p>
-                    </div>
-                  </HoverCardContent>
-                </HoverCard>
-              </div>
-              <div className="relative z-0 w-full mb-5 group">
-                <Label htmlFor="register_cPassword">Confirm Password</Label>
-                <Input
-                  id="register_cPassword"
-                  type="password"
-                  name="confirmPassword"
-                  value={userData.confirmPassword}
-                  onChange={handleInputChange}
-                  placeholder="Confirm password"
-                  required
-                  autoComplete="new-password"
-                />
-              </div>
-            </div>
-            <CoolMode options={{ particleCount: 50 }}>
-              <Button size="lg">
-                {isLoading ? "Registering..." : "Register"}
               </Button>
             </CoolMode>
           </form>
