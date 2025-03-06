7b0d716c73ba96185919908b5ce7a1b3a3134a52
Tina Huynh <tinamchuynh@gmail.com>
2025-03-05 20:11:33 -0800

    feat: replace Carousels with SlantedHero component on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import { Carousels } from "@/components/Carousel";
+import SlantedHero from "@/components/SlantedHero";
 
 export default function Home() {
   return (
     <>
       <main className="w-11/12 md:w-10/12 mx-auto">
-        <h1>Home Page</h1>
-        <p>Welcome to the Home Page</p>
-        <Carousels />
+        <SlantedHero />
       </main>
     </>
   );
