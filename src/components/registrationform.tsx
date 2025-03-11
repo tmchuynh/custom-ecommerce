a98b1894823fa241bb3a5909da1c0de3b0e5b079
Tina Huynh <tinamchuynh@gmail.com>
2025-03-10 22:39:23 -0700

    feat: clean up imports in RegistrationForm component

--- src/components/registrationform.tsx
+++ src/components/registrationform.tsx
@@ -1,13 +1,7 @@
 "use client";
-
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 export function RegistrationForm({
   className,
