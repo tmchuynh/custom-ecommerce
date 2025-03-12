ee16fda1b004593615b4bc01a9d25b904159739e
Tina Huynh <tinamchuynh@gmail.com>
2025-03-12 02:06:12 -0700

    feat: add Google Maps API key to configuration for mapping services

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,7 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_EMAILJS_SERVICE_ID: "service_8nwkxet",
     NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: "template_bsky8la",
     NEXT_PUBLIC_EMAILJS_PUBLIC_KEY: "qeV_W2IMId2NMUL5W",
+    GOOGLE_MAPS_API_KEY: "AIzaSyDQsUfinJeNTfOqqP1LHxrQOBtecJCzrkg",
   },
   images: {
     remotePatterns: imageHosts.map((hostname) => ({
