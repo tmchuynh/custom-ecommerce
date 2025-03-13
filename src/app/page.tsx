f66f6c1b52816f24fe9cec3e9eb5908263da38ac
Tina Huynh <tinamchuynh@gmail.com>
2025-03-13 00:05:46 -0700

    feat: add policies section to HomePage for enhanced user information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import {
   navigations,
   offers,
   perks,
+  policies,
   testimonials,
   trendingProducts,
 } from "@/lib/constants";
@@ -199,6 +200,26 @@ export default function HomePage() {
                 );
               }
             })}
+          </div>
+        </section>
+
+        {/* Policies section */}
+        <section aria-labelledby="policy-heading" className="mt-16 lg:mt-24">
+          <h2 id="policy-heading" className="sr-only">
+            Our policies
+          </h2>
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 lg:gap-x-8">
+            {policies.map((policy) => (
+              <div key={policy.name}>
+                <img alt="" src={policy.imageSrc} className="h-24 w-auto" />
+                <h3 className="mt-6 text-base font-medium text-gray-900">
+                  {policy.name}
+                </h3>
+                <p className="mt-3 text-base text-gray-500">
+                  {policy.description}
+                </p>
+              </div>
+            ))}
           </div>
         </section>
 
