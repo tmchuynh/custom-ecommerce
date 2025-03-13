2dca4717b6bb548dd75b439fedcc8f1d1bc9f6bc
Tina Huynh <tinamchuynh@gmail.com>
2025-03-12 21:44:25 -0700

    feat: refactor Footer component to use centralized footerNavigation constants

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,33 +1,4 @@
-import { JSX, SVGProps } from "react";
-
-const footerNavigation = {
-  shop: [
-    { name: "Bags", href: "#" },
-    { name: "Tees", href: "#" },
-    { name: "Objects", href: "#" },
-    { name: "Home Goods", href: "#" },
-    { name: "Accessories", href: "#" },
-  ],
-  company: [
-    { name: "Who we are", href: "#" },
-    { name: "Sustainability", href: "#" },
-    { name: "Press", href: "#" },
-    { name: "Careers", href: "#" },
-    { name: "Terms & Conditions", href: "#" },
-    { name: "Privacy", href: "#" },
-  ],
-  account: [
-    { name: "Manage Account", href: "#" },
-    { name: "Returns & Exchanges", href: "#" },
-    { name: "Redeem a Gift Card", href: "#" },
-  ],
-  connect: [
-    { name: "Contact Us", href: "#" },
-    { name: "Facebook", href: "#" },
-    { name: "Instagram", href: "#" },
-    { name: "Pinterest", href: "#" },
-  ],
-};
+import { footerNavigation } from "@/lib/constants";
 
 export default function Footer() {
   return (
