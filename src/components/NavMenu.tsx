9b176cc0432a0087f5444a80b911a125c023763a
Tina Huynh <tinamchuynh@gmail.com>
2025-03-14 01:07:14 -0700

    fix: adjust z-index for NavMenu and update currency selector visibility on mobile

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -105,7 +105,7 @@ export default function NavMenu() {
   };
 
   return (
-    <div className="relative z-50 shadow-sm">
+    <div className="relative z-20 shadow-sm">
       {/* Mobile menu */}
       <Dialog open={open} onClose={setOpen} className="relative z-40 lg:hidden">
         <DialogBackdrop
@@ -238,43 +238,44 @@ export default function NavMenu() {
 
       <nav>
         {/* Top navigations */}
-        <div className="bg-muted">
-          <div className="mx-auto flex h-10 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-            {/* Currency selector */}
-            <form>
-              <div className="-ml-2 inline-grid grid-cols-1">
-                <SelectGroup>
-                  <Select
-                    value={selectedCurrency}
-                    onValueChange={setSelectedCurrency}
-                  >
-                    <SelectTrigger className="max-w-fit pr-7 pl-2 text-left text-base font-medium sm:text-sm/6 focus-visible:outline-none border-none focus-visible:ring-0">
-                      <SelectValue placeholder="Select Currency" />
-                    </SelectTrigger>
+        <div className="bg-muted"></div>
+        <div className="mx-auto flex h-10 max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+          {/* Currency selector */}
+          <form className="hidden md:block">
+            <div className="-ml-2 inline-grid grid-cols-1">
+              <SelectGroup>
+                <Select
+                  value={selectedCurrency}
+                  onValueChange={setSelectedCurrency}
+                >
+                  <SelectTrigger className="max-w-fit pr-7 pl-2 text-left text-base font-medium sm:text-sm/6 focus-visible:outline-none border-none focus-visible:ring-0">
+                    <SelectValue placeholder="Select Currency" />
+                  </SelectTrigger>
 
-                    <SelectContent>
-                      {currencies.map((currency) => (
-                        <SelectItem
-                          className="focus:bg-muted focus:text-muted-foreground"
-                          key={currency}
-                          value={currency}
-                        >
-                          {currency}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                </SelectGroup>
-              </div>
-            </form>
+                  <SelectContent>
+                    {currencies.map((currency) => (
+                      <SelectItem
+                        className="focus:bg-muted focus:text-muted-foreground"
+                        key={currency}
+                        value={currency}
+                      >
+                        {currency}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </SelectGroup>
+            </div>
+          </form>
 
-            <div className="flex items-center space-x-6">
-              <a href="#" className="text-sm font-medium">
-                Sign in
-              </a>
-              <a href="#" className="text-sm font-medium">
-                Create an account
-              </a>
+          <div className="flex items-center space-x-6">
+            <a href="#" className="text-sm font-medium">
+              Sign in
+            </a>
+            <a href="#" className="text-sm font-medium">
+              Create an account
+            </a>
+            <div className="hidden md:block">
               <ThemeToggle />
             </div>
           </div>
