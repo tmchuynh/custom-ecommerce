e1dd33f22bce97fe3b9dfe5afd73b148e11abc07
Tina Huynh <tinamchuynh@gmail.com>
2025-03-20 20:40:39 -0700

    refactor: add clearCart functionality and improve cart item display in CartPage

--- src/app/shopping_cart/page.tsx
+++ src/app/shopping_cart/page.tsx
@@ -27,8 +27,13 @@ import { JSX } from "react";
  * @returns {JSX.Element} The rendered shopping cart page.
  */
 const CartPage = (): JSX.Element => {
-  const { cartItems, updateQuantity, removeFromCart, getTotalPrice } =
-    useCart(); // Access cart data
+  const {
+    cartItems,
+    updateQuantity,
+    removeFromCart,
+    getTotalPrice,
+    clearCart,
+  } = useCart(); // Access cart data
 
   /**
    * Handles the update of an item's quantity in the shopping cart.
@@ -54,56 +59,63 @@ const CartPage = (): JSX.Element => {
       {cartItems.length === 0 ? (
         <p className="text-xl text-center">Your cart is empty.</p>
       ) : (
-        <div className="space-y-8">
-          {cartItems.map((item, index) => (
-            <div
-              key={item.id}
-              className={cn("md:grid md:grid-cols-4 lg:grid-cols-7 pb-10", {
-                "border-b": index !== cartItems.length - 1,
-              })}
-            >
-              <div className="md:grid md:grid-cols-3 md:col-span-3 lg:col-span-5 lg:grid-cols-4 xl:grid-cols-7">
-                {/* <Image
+        <>
+          <div>
+            <Button variant={"destructive"} onClick={() => clearCart()}>
+              Remove
+            </Button>
+          </div>
+          <div className="space-y-8">
+            {cartItems.map((item, index) => (
+              <div
+                key={item.id}
+                className={cn("md:grid md:grid-cols-4 lg:grid-cols-7 pb-10", {
+                  "border-b": index !== cartItems.length - 1,
+                })}
+              >
+                <div className="md:grid md:grid-cols-3 md:col-span-3 lg:col-span-5 lg:grid-cols-4 xl:grid-cols-7">
+                  {/* <Image
                     src={item.imageSrc}
                     alt={item.name}
                     width={175}
                     height={175}
                   /> */}
-                <Skeleton className="h-full w-full rounded-xl hidden md:flex col-span-1 xl:col-span-2" />
-                <div className="flex flex-col justify-center gap-y-3 col-span-2 xl:col-span-5">
-                  <p className="text-lg font-medium">{item.name}</p>
-                  <p className="text-sm font-medium">{item.description}</p>
+                  <Skeleton className="h-60 w-full rounded-xl hidden md:flex col-span-1 xl:col-span-2" />
+                  <div className="flex flex-col justify-center gap-y-3 col-span-2 xl:col-span-5 pl-9">
+                    <p className="text-lg font-medium">{item.name}</p>
+                    <p className="text-sm font-medium">{item.description}</p>
+                  </div>
                 </div>
-              </div>
 
-              <div className="flex items-center justify-center md:justify-end space-x-4 py-9 md:col-span-1 lg:col-span-2">
-                <input
-                  type="number"
-                  value={item.quantity}
-                  onChange={(e) =>
-                    handleUpdateQuantity(item.id, parseInt(e.target.value))
-                  }
-                  className="w-16 text-center border rounded-md"
-                />
-                <div className="text-lg font-medium">
-                  ${Number(item.price) * item.quantity}
+                <div className="flex items-center justify-center md:justify-end space-x-4 py-9 md:col-span-1 lg:col-span-2">
+                  <input
+                    type="number"
+                    value={item.quantity}
+                    onChange={(e) =>
+                      handleUpdateQuantity(item.id, parseInt(e.target.value))
+                    }
+                    className="w-16 text-center border rounded-md"
+                  />
+                  <div className="text-lg font-medium">
+                    ${Number(item.price) * item.quantity}
+                  </div>
+                  <Button
+                    variant={"destructive"}
+                    onClick={() => handleRemoveItem(item.id)}
+                  >
+                    Remove
+                  </Button>
                 </div>
-                <Button
-                  variant={"destructive"}
-                  onClick={() => handleRemoveItem(item.id)}
-                >
-                  Remove
-                </Button>
               </div>
-            </div>
-          ))}
-          <div className="flex justify-between items-center pt-2">
-            <div className="text-lg font-medium">Total:</div>
-            <div className="text-xl font-bold">
-              ${getTotalPrice().toFixed(2)}
+            ))}
+            <div className="flex justify-between items-center pt-2">
+              <div className="text-lg font-medium">Total:</div>
+              <div className="text-xl font-bold">
+                ${getTotalPrice().toFixed(2)}
+              </div>
             </div>
           </div>
-        </div>
+        </>
       )}
     </div>
   );
