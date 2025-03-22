6adad6dbd905c2804518c213721959385a67c0e6
Tina Huynh <tinamchuynh@gmail.com>
2025-03-22 00:31:23 -0700

    feat: refactor QuantityButtons component to use product name for cart operations and improve item retrieval

--- src/components/Quantity.tsx
+++ src/components/Quantity.tsx
@@ -2,27 +2,36 @@ import { useCart } from "@/app/context/cartContext";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { ProductType } from "@/lib/types";
 
-function QuantityButtons({ itemId }: { itemId: number }) {
-  const { cartItems, updateQuantity, removeFromCart } = useCart();
-  const foundItem = cartItems.find((item) => item.id === itemId);
+function QuantityButtons({ product }: { product: ProductType }) {
+  const { updateQuantity, removeFromCart, itemExistsInCart, getCartItem } =
+    useCart();
+  const foundItem = itemExistsInCart(product?.name);
 
-  if (!foundItem) return null;
+  if (!foundItem) {
+    return null;
+  }
+
+  const cartItem = getCartItem(product.name);
 
   const handleIncrement = () => {
-    updateQuantity(itemId, foundItem.quantity + 1);
+    updateQuantity(product.name, product.quantity + 1);
   };
 
   const handleDecrement = () => {
-    updateQuantity(itemId, foundItem.quantity - 1);
+    updateQuantity(product.name, product.quantity - 1);
   };
 
   return (
     <div className="flex items-center gap-3">
       <Button onClick={handleDecrement}>-</Button>
-      <Input readOnly value={foundItem.quantity} className="w-12 text-center" />
+      <Input readOnly value={cartItem?.quantity} className="w-12 text-center" />
       <Button onClick={handleIncrement}>+</Button>
-      <Button variant="destructive" onClick={() => removeFromCart(itemId)}>
+      <Button
+        variant="destructive"
+        onClick={() => removeFromCart(product.name)}
+      >
         Remove
       </Button>
     </div>
