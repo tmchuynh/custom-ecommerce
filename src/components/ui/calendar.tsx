4714944ee21967c9f6bd060b874f0a16ee9cc0d2
Tina Huynh <tinamchuynh@gmail.com>
2025-03-22 17:26:34 -0700

    refactor: enhance formatting and improve component structure in Calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import * as React from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { DayPicker } from "react-day-picker";
 
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>
+export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
 function Calendar({
   className,
@@ -59,18 +59,33 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
-      }}
+      components={
+        {
+          Caption: (props: any) => (
+            <div className="flex justify-between items-center">
+              <button
+                type="button"
+                onClick={props.onPrev}
+                className="h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
+              >
+                <ChevronLeft className="h-4 w-4" />
+              </button>
+              <span className="text-sm font-medium">{props.label}</span>
+              <button
+                type="button"
+                onClick={props.onNext}
+                className="h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
+              >
+                <ChevronRight className="h-4 w-4" />
+              </button>
+            </div>
+          ),
+        } as any
+      }
       {...props}
     />
-  )
+  );
 }
-Calendar.displayName = "Calendar"
+Calendar.displayName = "Calendar";
 
-export { Calendar }
+export { Calendar };
