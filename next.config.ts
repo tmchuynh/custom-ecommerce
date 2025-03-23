ed30b6fe8ad37fa2f54ab3f39c80810d1309ba7d
Tina Huynh <tinamchuynh@gmail.com>
2025-03-23 16:17:24 -0700

    feat: update imageHosts in next.config.ts by removing unused hosts and adding media.istockphoto.com

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,10 @@
 import type { NextConfig } from "next";
 
 const imageHosts = [
-  "placehold.co",
-  "tailwindui.com",
   "tailwindcss.com",
   "images.unsplash.com",
-  "hips.hearstapps.com",
-  "previews.123rf.com",
-  "www.websiteclosers.com",
-  "media.gq.com",
-  "i5.walmartimages.com",
-  "fashionisers.com",
-  "png.pngtree.com",
   "plus.unsplash.com",
-  "www.shutterstock.com",
+  "media.istockphoto.com",
 ];
 
 const nextConfig: NextConfig = {
