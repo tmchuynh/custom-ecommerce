2ea85c264c016b86e7d1214fac2c839a3098ff7e
Tina Huynh <tinamchuynh@gmail.com>
2025-03-22 17:26:40 -0700

    refactor: replace img tags with Image component for better performance in LogoCloud

--- src/components/ui/logoCloud.tsx
+++ src/components/ui/logoCloud.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function LogoCloud() {
   return (
     <div className="py-24 sm:py-32">
@@ -5,43 +7,41 @@ export default function LogoCloud() {
         <h2 className="text-2xl font-semibold mb-4">
           Trusted by the world’s most innovative teams
         </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-          <img
-            alt="Transistor"
-            src="https://tailwindui.com/plus-assets/img/logos/158x48/transistor-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="Reform"
-            src="https://tailwindui.com/plus-assets/img/logos/158x48/reform-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="Tuple"
-            src="https://tailwindui.com/plus-assets/img/logos/158x48/tuple-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="SavvyCal"
-            src="https://tailwindui.com/plus-assets/img/logos/158x48/savvycal-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-          />
-          <img
-            alt="Statamic"
-            src="https://tailwindui.com/plus-assets/img/logos/158x48/statamic-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-          />
-        </div>
+        <Image
+          alt="Transistor"
+          src="https://tailwindui.com/plus-assets/img/logos/158x48/transistor-logo-gray-900.svg"
+          width={158}
+          height={48}
+          className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+        />
+        <Image
+          alt="Reform"
+          src="https://tailwindui.com/plus-assets/img/logos/158x48/reform-logo-gray-900.svg"
+          width={158}
+          height={48}
+          className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+        />
+        <Image
+          alt="Tuple"
+          src="https://tailwindui.com/plus-assets/img/logos/158x48/tuple-logo-gray-900.svg"
+          width={158}
+          height={48}
+          className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+        />
+        <Image
+          alt="SavvyCal"
+          src="https://tailwindui.com/plus-assets/img/logos/158x48/savvycal-logo-gray-900.svg"
+          width={158}
+          height={48}
+          className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+        />
+        <Image
+          alt="Statamic"
+          src="https://tailwindui.com/plus-assets/img/logos/158x48/statamic-logo-gray-900.svg"
+          width={158}
+          height={48}
+          className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+        />
       </div>
     </div>
   );
