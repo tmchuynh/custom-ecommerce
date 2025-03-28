4c83f9242b311ee49130cc1e323c4a58bf29257a
Tina Huynh <tinamchuynh@gmail.com>
2025-03-27 23:22:40 -0700

    feat: optimize Button component rendering with memoized className calculation

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -43,13 +43,12 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      />
+    const buttonClassName = React.useMemo(
+      () => cn(buttonVariants({ variant, size, className })),
+      [variant, size, className]
     );
+
+    return <Comp className={buttonClassName} ref={ref} {...props} />;
   }
 );
 Button.displayName = "Button";
