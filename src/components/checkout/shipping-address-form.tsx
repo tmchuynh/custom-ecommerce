39d00bd885d959c25c0b2b18a7660e3871cebbfd
Tina Huynh <tinamchuynh@gmail.com>
2025-03-27 22:45:50 -0700

    feat: enhance shipping address form with improved styling and validation messages for all fields

--- src/components/checkout/shipping-address-form.tsx
+++ src/components/checkout/shipping-address-form.tsx
@@ -38,7 +38,7 @@ const ShippingAddressForm = ({
       <CardContent>
         <div className="space-y-4">
           <div>
-            <FormLabel>Street Address</FormLabel>
+            <FormLabel className="my-2 ml-2">Street Address</FormLabel>
             <Input
               placeholder="123 Main St"
               value={shippingAddress}
@@ -47,53 +47,101 @@ const ShippingAddressForm = ({
               className={
                 touchedFields.shippingAddress && formErrors.shippingAddress
                   ? "border-red-500"
-                  : ""
+                  : "border border-border"
               }
             />
             {touchedFields.shippingAddress && formErrors.shippingAddress && (
-              <FormMessage>{formErrors.shippingAddress}</FormMessage>
+              <FormMessage className="mt-3 mx-2">
+                {formErrors.shippingAddress}
+              </FormMessage>
             )}
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div>
-              <FormLabel>City</FormLabel>
+              <FormLabel className="my-2 ml-2">City</FormLabel>
               <Input
                 placeholder="City"
                 value={shippingCity}
                 onChange={(e) => setShippingCity(e.target.value)}
+                onBlur={() => handleBlur("shippingCity")}
+                className={
+                  touchedFields.shippingCity && formErrors.shippingCity
+                    ? "border-red-500"
+                    : "border border-border"
+                }
               />
+              {touchedFields.shippingCity && formErrors.shippingCity && (
+                <FormMessage className="mt-3 mx-2">
+                  {formErrors.shippingCity}
+                </FormMessage>
+              )}
             </div>
 
             <div>
-              <FormLabel>State/Province</FormLabel>
+              <FormLabel className="my-2 ml-2">State/Province</FormLabel>
               <Input
                 placeholder="State/Province"
                 value={shippingState}
                 onChange={(e) => setShippingState(e.target.value)}
+                onBlur={() => handleBlur("shippingState")}
+                className={
+                  touchedFields.shippingState && formErrors.shippingState
+                    ? "border-red-500"
+                    : "border border-border"
+                }
               />
+              {touchedFields.shippingState && formErrors.shippingState && (
+                <FormMessage className="mt-3 mx-2">
+                  {formErrors.shippingState}
+                </FormMessage>
+              )}
             </div>
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             <div>
-              <FormLabel>ZIP/Postal Code</FormLabel>
+              <FormLabel className="my-2 ml-2">ZIP/Postal Code</FormLabel>
               <Input
                 placeholder="ZIP/Postal Code"
                 value={shippingZip}
                 onChange={(e) => setShippingZip(e.target.value)}
+                onBlur={() => handleBlur("shippingZip")}
+                className={
+                  touchedFields.shippingZip && formErrors.shippingZip
+                    ? "border-red-500"
+                    : "border border-border"
+                }
               />
+              {touchedFields.shippingZip && formErrors.shippingZip && (
+                <FormMessage className="mt-3 mx-2">
+                  {formErrors.shippingZip}
+                </FormMessage>
+              )}
             </div>
 
             <div>
-              <FormLabel>Country</FormLabel>
+              <FormLabel className="my-2 ml-2">Country</FormLabel>
               <Select
                 value={shippingCountry}
                 onValueChange={setShippingCountry}
               >
-                <SelectTrigger>
+                <SelectTrigger
+                  onBlur={() => handleBlur("shippingCountry")}
+                  className={
+                    touchedFields.shippingCountry && formErrors.shippingCountry
+                      ? "border-red-500"
+                      : "border border-border"
+                  }
+                >
                   <SelectValue placeholder="Select a country" />
                 </SelectTrigger>
+                {touchedFields.shippingCountry &&
+                  formErrors.shippingCountry && (
+                    <FormMessage className="mt-3 mx-2">
+                      {formErrors.shippingCountry}
+                    </FormMessage>
+                  )}
                 <SelectContent>
                   <SelectGroup>
                     {currencyCountries
