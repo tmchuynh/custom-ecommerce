723d9911a8657d0e46baf605a00d76b5884e589a
Tina Huynh <tinamchuynh@gmail.com>
2025-03-30 14:31:13 -0700

    feat: update configuration and dependencies for improved environment variable management and support for SVG loading

--- next.config.ts
+++ next.config.ts
@@ -10,11 +10,26 @@ const imageHosts = [
 ];
 
 const nextConfig: NextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
   env: {
     NEXT_PUBLIC_EMAILJS_SERVICE_ID: "service_8nwkxet",
     NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: "template_bsky8la",
     NEXT_PUBLIC_EMAILJS_PUBLIC_KEY: "qeV_W2IMId2NMUL5W",
     GOOGLE_MAPS_API_KEY: "AIzaSyDQsUfinJeNTfOqqP1LHxrQOBtecJCzrkg",
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
   },
   images: {
     remotePatterns: imageHosts.map((hostname) => ({
