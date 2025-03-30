a8a74a768e5e953201aa5362a51438e8cfe7f442
Tina Huynh <tinamchuynh@gmail.com>
2025-03-29 22:43:34 -0700

    feat: restructure provider hierarchy to enhance context management and maintainability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
-import { CustomerProvider } from "@/app/context/customerContext";
 import BackToTop from "@/components/BackToTop";
 import BreadcrumbWrapper from "@/components/BreadcrumbWrapper";
 import Footer from "@/components/Footer";
 import NavMenu from "@/components/NavMenu";
 import NavTopMenu from "@/components/NavTopMenu";
 import { Toaster } from "sonner";
-import { AuthProvider } from "./context/authContext";
 import { NotFoundProvider } from "./context/NotFoundContext";
 import "./globals.css";
 import { Providers } from "./providers";
@@ -20,26 +18,22 @@ export default function RootLayout({
       <body className="flex flex-col min-h-screen">
         <Providers>
           <NotFoundProvider>
-            <AuthProvider>
-              <CustomerProvider>
-                <NavTopMenu />
-                <NavMenu />
-                <main className="flex-grow">
-                  <BreadcrumbWrapper />
-                  {children}
-                  <Footer />
-                </main>
-                <BackToTop />
-                <Toaster
-                  position="top-center"
-                  toastOptions={{
-                    style: {
-                      marginTop: "200px",
-                    },
-                  }}
-                />
-              </CustomerProvider>
-            </AuthProvider>
+            <NavTopMenu />
+            <NavMenu />
+            <main className="flex-grow">
+              <BreadcrumbWrapper />
+              {children}
+              <Footer />
+            </main>
+            <BackToTop />
+            <Toaster
+              position="top-center"
+              toastOptions={{
+                style: {
+                  marginTop: "200px",
+                },
+              }}
+            />
           </NotFoundProvider>
         </Providers>
       </body>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,22 +1,28 @@
 "use client";
 
 import React from "react";
-import { CurrencyProvider } from "./context/currencyContext";
-import { WishlistProvider } from "./context/wishlistContext";
+import { AuthProvider } from "./context/authContext";
 import { CartProvider } from "./context/cartContext";
-import { ProductProvider } from "./context/productContext";
+import { CurrencyProvider } from "./context/currencyContext";
+import { CustomerProvider } from "./context/customerContext";
 import { PaymentProvider } from "./context/paymentContext";
+import { ProductProvider } from "./context/productContext";
+import { WishlistProvider } from "./context/wishlistContext";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CurrencyProvider>
-      <ProductProvider>
-        <WishlistProvider>
-          <PaymentProvider>
-            <CartProvider>{children}</CartProvider>
-          </PaymentProvider>
-        </WishlistProvider>
-      </ProductProvider>
-    </CurrencyProvider>
+    <AuthProvider>
+      <CustomerProvider>
+        <CurrencyProvider>
+          <ProductProvider>
+            <WishlistProvider>
+              <PaymentProvider>
+                <CartProvider>{children}</CartProvider>
+              </PaymentProvider>
+            </WishlistProvider>
+          </ProductProvider>
+        </CurrencyProvider>
+      </CustomerProvider>
+    </AuthProvider>
   );
 }
