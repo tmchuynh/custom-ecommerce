8a662fdd5d348fefe351e61d7fb83e9a718c339e
Tina Huynh <tinamchuynh@gmail.com>
2025-03-31 18:52:52 -0700

    feat: redesign NotFound component with improved layout and navigation links

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,108 +1,153 @@
-"use client";
-import DynamicButton from "@/components/ui/button-dynamic";
+import { JSX, SVGProps } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { useEffect, useState, type JSX } from "react";
-import { FaPhoneAlt } from "react-icons/fa";
-import { IoMdClose } from "react-icons/io";
+import {
+  FaChevronRight,
+  FaNewspaper,
+  FaRss,
+  FaShoppingBag,
+} from "react-icons/fa";
+import { FaBook } from "react-icons/fa6";
 
-/**
- * NotFound component renders a 404 error page with a countdown timer that redirects to the home page.
- * It provides options to cancel the redirection, go back to the home page immediately, or contact support.
- *
- * @component
- * @example
- * return (
- *   <NotFound />
- * )
- *
- * @returns {JSX.Element} The rendered 404 error page component.
- *
- * @remarks
- * - The component uses a countdown timer to automatically redirect the user to the home page after 10 seconds.
- * - Users can cancel the redirection by clicking the "Cancel Redirection" button.
- * - The "Go back home" button redirects the user to the home page immediately.
- * - The "Contact support" button opens the default mail client to send an email to support.
- *
- * @dependencies
- * - `useRouter` from `next/navigation` for navigation.
- * - `useState` and `useEffect` from `react` for state management and side effects.
- * - `DynamicButton` component for rendering buttons with different styles and actions.
- * - `IoMdClose` and `FaPhoneAlt` icons from `react-icons` for button icons.
- */
-const NotFound = (): JSX.Element => {
-  const router = useRouter();
-  const [countdown, setCountdown] = useState(10);
-  const [redirect, setRedirect] = useState(true);
+const links = [
+  {
+    name: "Documentation",
+    href: "#",
+    description: "Learn how to integrate our tools with your app.",
+    icon: FaBook,
+  },
+  {
+    name: "API Reference",
+    href: "#",
+    description: "A complete API reference for our libraries.",
+    icon: FaShoppingBag,
+  },
+  {
+    name: "Guides",
+    href: "#",
+    description: "Installation guides that cover popular setups.",
+    icon: FaNewspaper,
+  },
+  {
+    name: "Blog",
+    href: "#",
+    description: "Read our latest news and articles.",
+    icon: FaRss,
+  },
+];
+const social = [
+  {
+    name: "X",
+    href: "#",
+    icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path d="M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z" />
+      </svg>
+    ),
+  },
+  {
+    name: "GitHub",
+    href: "#",
+    icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    name: "Instagram",
+    href: "#",
+    icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+      <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+        <path
+          fillRule="evenodd"
+          d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
+  },
+];
 
-  useEffect(() => {
-    if (!redirect) return;
-
-    const timer = setInterval(() => {
-      setCountdown((prev) => {
-        if (prev <= 1) {
-          clearInterval(timer);
-          return 0;
-        }
-        return prev - 1;
-      });
-    }, 1000);
-
-    const redirectTimer = setTimeout(() => {
-      router.push(`/`);
-    }, 10000);
-
-    return () => {
-      clearInterval(timer);
-      clearTimeout(redirectTimer);
-    };
-  }, [router, redirect]);
-
+export default function NotFound() {
   return (
-    <main className="grid place-items-center px-6 py-24 sm:py-32 lg:px-8 min-h-screen">
-      <div className="text-center">
-        <h1 className="text-4xl font-extrabold text-center mb-8">
-          404 Page not found
+    <div className="">
+      <Image
+        alt="Your Company"
+        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+        width={48}
+        height={48}
+        className="mx-auto h-10 w-auto sm:h-12"
+      />
+      <div className="mx-auto mt-20 max-w-2xl text-center sm:mt-24">
+        <p className="text-base/8 font-semibold text-accent">404</p>
+        <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-primary sm:text-6xl">
+          This page does not exist
         </h1>
-        <div className="mt-6 text-pretty text-lg font-medium sm:text-xl/8">
+        <p className="mt-6 text-lg font-medium text-pretty text-secondary sm:text-xl/8">
           Sorry, we couldn’t find the page you’re looking for.
-          {redirect && (
-            <p>
-              Redirecting you to the home page in{" "}
-              <span className="font-bold">{countdown}</span> seconds.
-            </p>
-          )}
-        </div>
-        <div className="mt-10 flex flex-col items-center justify-center gap-x-6">
-          <DynamicButton
-            text="Go back home"
-            onClick={() => {
-              router.push(`/`);
-            }}
-          />
-          {redirect && (
-            <DynamicButton
-              variant="outline"
-              icon={IoMdClose}
-              text="Cancel Redirection"
-              onClick={() => {
-                setRedirect(false);
-                setCountdown(0);
-              }}
-              iconClassName="group-hover:rotate-90"
-            />
-          )}
-          <DynamicButton
-            variant="outline"
-            text="Contact support"
-            onClick={() => router.push(`mailto:tina@iacafterschools.com`)}
-            icon={FaPhoneAlt}
-            iconClassName="group-hover:-rotate-90"
-          />
+        </p>
+      </div>
+      <div className="mx-auto mt-16 flow-root max-w-lg sm:mt-20">
+        <h2 className="sr-only">Popular pages</h2>
+        <ul role="list" className="-mt-6 divide-y border-b">
+          {links.map((link, linkIdx) => (
+            <li key={linkIdx} className="relative flex gap-x-6 py-6">
+              <div className="flex size-10 flex-none items-center justify-center rounded-lg ring-1 shadow-xs">
+                <link.icon
+                  aria-hidden="true"
+                  className="size-6 text-teritary"
+                />
+              </div>
+              <div className="flex-auto">
+                <h3 className="text-sm/6 font-semibold text-primary-900">
+                  <a href={link.href}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {link.name}
+                  </a>
+                </h3>
+                <p className="mt-2 text-sm/6 text-secondary">
+                  {link.description}
+                </p>
+              </div>
+              <div className="flex-none self-center">
+                <FaChevronRight
+                  aria-hidden="true"
+                  className="size-5 text-accent"
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+        <div className="mt-10 flex justify-center">
+          <a href="#" className="text-sm/6 font-semibold text-accent">
+            <span aria-hidden="true">&larr;</span> Back to home
+          </a>
         </div>
       </div>
-    </main>
+      <footer className="border-t py-6 sm:py-10">
+        <div className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-8 px-6 sm:flex-row lg:px-8">
+          <p className="text-sm/7 text-muted">
+            &copy; Your Company, Inc. All rights reserved.
+          </p>
+          <div className="hidden sm:block sm:h-7 sm:w-px sm:flex-none" />
+          <div className="flex gap-x-4">
+            {social.map((item, itemIdx) => (
+              <a
+                key={itemIdx}
+                href={item.href}
+                className="text-muted hover:text-accent"
+              >
+                <span className="sr-only">{item.name}</span>
+                <item.icon aria-hidden="true" className="size-6" />
+              </a>
+            ))}
+          </div>
+        </div>
+      </footer>
+    </div>
   );
-};
-
-export default NotFound;
+}
