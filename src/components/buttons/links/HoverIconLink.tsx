7007a5d35c14bc02d0458920d8e1091417117355
Tina Huynh <tinamchuynh@gmail.com>
2025-04-01 19:34:48 -0700

    fix: enhance HoverIconLink component with improved hover effects and styling

--- src/components/buttons/links/HoverIconLink.tsx
+++ src/components/buttons/links/HoverIconLink.tsx
@@ -9,10 +9,10 @@ interface HoverIconLinkProps {
 
 const HoverIconLink = ({ link, icon: Icon }: HoverIconLinkProps) => {
   return (
-    <div className="hover-icon-link flex items-center justify-between w-full">
-      {link}
+    <div className="flex items-center justify-between w-full group">
+      <span className="group-hover:underline underline-offset-2">{link}</span>
       <span className="icon">
-        <Icon />
+        <Icon className="hidden group-hover:inline-flex" />
       </span>
     </div>
   );
