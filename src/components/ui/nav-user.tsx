b7276fb3321efdb891726aed906b1c8378753b92
Tina Huynh <tinamchuynh@gmail.com>
2025-05-11 19:29:39 -0700

    refactor: reorder class names for consistency and improve readability in NavUser component

--- src/components/ui/nav-user.tsx
+++ src/components/ui/nav-user.tsx
@@ -10,15 +10,15 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { IoSparkles } from "react-icons/io5";
-import { CiLogout } from "react-icons/ci";
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+import { CiLogout } from "react-icons/ci";
 import { FaBell, FaChevronDown, FaCreditCard, FaIdBadge } from "react-icons/fa";
+import { IoSparkles } from "react-icons/io5";
 
 export function NavUser({
   user,
@@ -40,32 +40,32 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg">
+              <Avatar className="rounded-lg w-8 h-8">
                 <AvatarImage src={user.avatar} alt={user.name} />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="truncate font-semibold">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
+              <div className="flex-1 grid text-left text-sm leading-tight">
+                <span className="font-semibold truncate">{user.name}</span>
+                <span className="text-xs truncate">{user.email}</span>
               </div>
               <FaChevronDown className="ml-auto size-4" />
             </SidebarMenuButton>
           </DropdownMenuTrigger>
           <DropdownMenuContent
-            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+            className="rounded-lg w-(--radix-dropdown-menu-trigger-width) min-w-56"
             side={isMobile ? "bottom" : "right"}
             align="start"
             sideOffset={4}
           >
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
+                <Avatar className="rounded-lg w-8 h-8">
                   <AvatarImage src={user.avatar} alt={user.name} />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
+                <div className="flex-1 grid text-left text-sm leading-tight">
+                  <span className="font-semibold truncate">{user.name}</span>
+                  <span className="text-xs truncate">{user.email}</span>
                 </div>
               </div>
             </DropdownMenuLabel>
